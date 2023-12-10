d663138cd089982b68da5efb13e091bb6dd901e8
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2023-12-10 17:50:00 +0100

    Nuovo fattoriale complicato

---
+++ esercizi pre-thread/4bis/complicato/chiama_fattoriale1.sh
@@ -0,0 +1,6 @@
+RIS=1
+source ./fattoriale1.sh 5   # con source faccio eseguire le cose nella stessa bash quindi finché non finsice
+                            # l'esecuzione di questo script ("chiama_fattoriale1.sh") RIS esiste e quello
+                            # che viene eseguito in source la può leggere 
+
+echo $RIS
---
+++ esercizi pre-thread/4bis/complicato/fattoriale1.sh
@@ -0,0 +1,3 @@
+for (( CONTATORE=1 ; CONTATORE <= $1 ; CONTATORE=${CONTATORE}+1)) ; do 
+    (( RIS=${RIS}*${CONTATORE} )) 
+done
