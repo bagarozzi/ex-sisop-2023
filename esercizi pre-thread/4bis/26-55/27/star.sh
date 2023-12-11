456ead09bbfbc8423868e010a735bad362d9c460
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2023-12-11 15:20:24 +0100

    New explanation

--- esercizi pre-thread/4bis/26-55/27/star.sh
+++ esercizi pre-thread/4bis/26-55/27/star.sh
@@ -4,3 +4,7 @@ exec {FD}< ./asteristchi.txt
 while read -u ${FD} PRIMA SECONDA TERZA QUARTA QUINTA ; do
     echo "${QUARTA} ${TERZA}"
 done
+
+# in alternativa potevo lanciare lo script dandogli nello stdin il file:
+# ./star.sh < asterischi.txt
+# e poi eseguire la lettura finché in file non finiva controllandolo con if (( $? == 0 ))
