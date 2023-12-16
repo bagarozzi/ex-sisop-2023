e2548507a4cb3a473e33563fcf06c3c3ec7c2c65
bagabagabaga <federicobagattoni61@gmail.com>
2023-12-16 10:55:35 +0100

    Wrong exercise

--- esercizi thread/es0_strutture/strutture.c
+++ esercizi thread/es0_strutture/strutture.c
@@ -3,36 +3,48 @@
 #include <stdio.h>
 #include <pthread.h>
 
+#define NUM_THREADS 4
+
 struct thargs {
     int remainingChilds;
     char str[100];
     int index;
 };
 
-void func(void* p){
+void* func(void* p){
     struct thargs* arg = (struct thargs*)p;
     struct thargs* newArg;
     struct thargs* returnStruct;
     int i, rc;
-    pthread_t tid;
+    pthread_t vth[arg->remainingChilds];
 
-    sleep(1);
+    sleep(1000); /* Aspetta un secondo */
 
-    if(arg->remainingChilds > 1){
+    if(arg->remainingChilds > 1){ /* Controlla se deve fare altri figli */
         
-        for(i = 1; i < arg->remainingChilds; i++){
+        for(i = 0; i < arg->remainingChilds; i++){ /* Li crea tutti passando n-1 ed un index unico */
             newArg = malloc(sizeof(struct thargs));
             if(newArg == NULL){
                 printf("Malloc failed");
                 exit(-1);
             }
             newArg->remainingChilds = arg->remainingChilds - 1;
-            newArg->index = i - 1;
-            rc = pthread_create(&tid, NULL, func, (void*)newArg);
+            newArg->index = i;
+            rc = pthread_create(&vth[i - 1], NULL, func, (void*)newArg);
             if(rc){
                 printf("ERROR; return code from pthread_create() is %d\n",rc);
                 exit(-1);
             }
+        }
+
+        for(i = 0; i < arg->remainingChilds; i++){
+            rc = pthread_join(vth[i], (void**)&returnStruct);
+            if(rc){
+                printf("There was a problem with pthread_join: %d", rc);
+                exit(-1);
+            }
+            printf("Received: \"%s\"\n", returnStruct->str);
+            free(returnStruct);
         }
 
     }
@@ -43,5 +55,31 @@ void func(void* p){
 }
 
 int main(void){
+    struct thargs* arg;
+    int rc, i;
+    pthread_t vth[NUM_THREADS];
 
+    for(i = 0; i < NUM_THREADS; i++){
+        arg = malloc(sizeof(struct thargs));
+        arg->index = i;
+        arg->remainingChilds = NUM_THREADS - 1;
+
+        rc = pthread_create(&vth[i], NULL, func, (void*)arg);
+        if(rc){
+            printf("pthread_create failed: %d", rc);
+            exit(-1);
+        }
+    }
+
+    for(i = 0; i < NUM_THREADS; i++){
+        rc = pthread_join(vth[i], (void*)&arg);
+        if(rc){
+            printf("pthread_join failed: %d", rc);
+            exit(-1);
+        }
+        printf("Main received: \"%s\"", arg->str);
+    }
+
+    pthread_exit(NULL);
+    return 0;
 }
