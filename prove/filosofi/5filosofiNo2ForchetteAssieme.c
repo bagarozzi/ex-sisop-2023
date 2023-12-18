3c85b921a53c28bf6b833a83d8070ffac51cd917
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2023-12-18 11:39:34 +0100

    Filosofi, penso vada bene

--- prove/filosofi/5filosofiNo2ForchetteAssieme.c
+++ prove/filosofi/5filosofiNo2ForchetteAssieme.c
@@ -20,16 +20,27 @@
 #define NUMFILOSOFI 5
 
 /* variabili da proteggere */
-/* la forchetta a sinistra di un filosofo
-   ha lo stesso indice del filosofo.
-	 */
+/* la forchetta a sinistra di un filosofo ha lo stesso indice del filosofo */
 int forchettaoccupata[NUMFILOSOFI];
+int turno = 0;
 
 /* variabili per la sincronizzazione */
 pthread_mutex_t	 mutexForchetta[NUMFILOSOFI]; 
 
-/* gli indici  dei filosofi crescono in senso orario, da 0 a NUMFILOSOFI-1 
-*/
+/* gli indici  dei filosofi crescono in senso orario, da 0 a NUMFILOSOFI-1 */
+
+int indiceDestra(int indice){
+	return (indice+1)%NUMFILOSOFI;
+}
+
+int indiceSinistra(int indice){
+	if (indice == 0){
+		return NUMFILOSOFI-1;
+	}
+	else {
+		return (indice-1)%NUMFILOSOFI;
+	}
+}
 
 void *filosofo (void *arg) 
 { 
@@ -44,21 +55,25 @@ void *filosofo (void *arg)
 	while(1) {
 		int indiceforchetta1;
 		int indiceforchetta2;
-		printf("filosofo %s sta pensando \n", Flabel ); 
+		printf("filosofo %s sta pensando\n", Flabel ); 
 		fflush(stdout);
 		/* il filosofo pensa per 1/2 di sec piu o meno */
 		DBGnanosleep( 500000000+indice*1000000, Flabel );
 
 		/* il filosofo prende le due forchette NON IN MANIERA ATOMICA */
 		/* AGGIUNGERE CODICE */
-
-
-
-
-
-
+		if(turno == indice){ /* è il suo turno di prendere prima la forchetta di sinistra */
+			pthread_mutex_lock(&mutexForchetta[indice]);
+			turno = (turno + 1)%NUMFILOSOFI; /* Cambio il turno così sarà un altro filosofo a prendere un altra forchetta*/
+			printf("Filsofo %s vuole la forchetta %d \n", Flabel, indiceDestra(indice));
+			pthread_mutex_lock(&mutexForchetta[indiceDestra(indice)]);
+		}
+		else { /* se no prendo prima la forchetta di destra */
+			pthread_mutex_lock(&mutexForchetta[indiceDestra(indice)]);
+			printf("Filsofo %s vuole la forchetta %d \n", Flabel, indice);
+			pthread_mutex_lock(&mutexForchetta[indice]);
+		}
 		/* FINE AGGIUNTA CODICE */
-
 		/*  FILOSOFO MANGIA */
 		printf("filosofo %s MANGIA \n", Flabel ); 
 		fflush(stdout);
@@ -66,17 +81,11 @@ void *filosofo (void *arg)
 		DBGnanosleep( 11000000000+indice*1000000, Flabel );
 		printf("filosofo %s FINISCE DI MANGIARE \n", Flabel ); 
 		fflush(stdout);
-
 		/* il filosofo rilascia le forchette */
 		/* AGGIUNGERE CODICE */
-
-
-
-
-
-
+		pthread_mutex_unlock(&mutexForchetta[indice]);
+		pthread_mutex_unlock(&mutexForchetta[indiceDestra(indice)]);
 		/* FINE AGGIUNTA CODICE */
-
 	}
 	pthread_exit(NULL); 
 } 
